d0d5fe1c4ec2766d3d1388fc3ad4b97712960508
gruf <gruf@codedrop.cc>
2021-10-08 13:51:31 +0600

    wip collection page

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,27 +1,27 @@
-import './collection.scss';
+import styles from './collection.module.scss';
 import BgShape1 from '../../components/BgShapes/BgShape1';
 import { SelectedProjects, ProjectsGrid } from '../../components/Projects';
 import ContactForm from '../../components/ContactForm';
 
 function CollectionPage() {
   return (
-    <div className={'wrapper collection'}>
-      <BgShape1/>
-      <section className={'first'}>
+    <div className={styles.content}>
+      <BgShape1 classNames={styles.bgShape} />
+      <section className={styles.first}>
         <h1>Building Bigger Ideas Together</h1>
         <p>Everyone working on your project is fully invested in its success. We care just as much
           about how your software looks and performs as we do about the problem that it’s solving.
           We work, learn, and celebrate together, measuring success by the duration of our long-term
           partnerships.</p>
       </section>
-      <section className="projects-main">
+      <section className={styles.projectsMain}>
         <SelectedProjects/>
       </section>
-      <section className="projects-secondary">
+      <section className={styles.projectsSecondary}>
         <ProjectsGrid/>
       </section>
-      <section className="start-project">
-        <div className="sub">work with us</div>
+      <section className={styles.startProject}>
+        <div className={styles.sub}>work with us</div>
         <h2>Start a Project</h2>
         <ContactForm/>
       </section>
