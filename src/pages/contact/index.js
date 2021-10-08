c38200a94753716aebce04b927ec3384c7f926c1
gruf <gruf@codedrop.cc>
2021-10-08 14:46:46 +0600

    rename page files

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,1 +1,25 @@
-export { default } from './Contact';
+import styles from './contact.module.scss'
+import ContactForm from '../../components/ContactForm';
+import BgTriangleBigLeft from '../../components/BgShapes/BgTriangleBigLeft';
+
+function ContactPage() {
+  return (
+    <>
+      <BgTriangleBigLeft classNames={styles.bgBigTriangleLeftShape} />
+      <div className={styles.content}>
+        <section className={styles.wrap}>
+          <div className={styles.left}>
+            <h1>Contact Us</h1>
+            <p>
+              Tell us about your project.<br/>
+              We'll back to you within 24 hrs.
+            </p>
+          </div>
+          <ContactForm/>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default ContactPage;
