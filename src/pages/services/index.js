a96adaefa70555ebc03d7d84ec948dbbbed793bb
gruf <gruf@codedrop.cc>
2021-10-13 17:59:37 +0600

    add services list

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -2,10 +2,98 @@ import styles from './services.module.scss';
 
 import BgShape1 from '../../components/BgShapes/BgShape1';
 import BgShapeBottom from '../../components/BgShapes/BgShapeBottom';
-import { BlueButton, DecoratedLink } from '../../components/Buttons';
-import Capability from '../../components/Capability';
-import { SelectedProjects } from '../../components/Projects';
-import cx from 'classnames'
+import cx from 'classnames';
+
+const data = [
+  {
+    anchor: 'development',
+    title: 'Development',
+    icon: '/images/icons/service-development-icon.svg',
+    iconAlt: 'service-development-icon',
+    text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.',
+    points: [
+      {
+        title: 'WEB DEVELOPMENT',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'system integration',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'SYSTEMS ARCHITECTURE',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'QUALITY ASSURANCE',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      }
+    ]
+  },
+  {
+    anchor: 'design',
+    title: 'Design',
+    icon: '/images/icons/service-design-icon.svg',
+    iconAlt: 'service-design-icon',
+    text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.',
+    points: [
+      {
+        title: 'research',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'behavior design',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'service design',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'product design',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'design strategy',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      }
+    ]
+  },
+  {
+    anchor: 'servers',
+    title: 'Cloud [?] & DevOps',
+    icon: '/images/icons/service-servers-icon.svg',
+    iconAlt: 'service-servers-icon',
+    text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.',
+    points: [
+      {
+        title: 'migration',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      }
+    ]
+  },
+  {
+    anchor: 'support',
+    title: 'Support',
+    icon: '/images/icons/service-support-icon.svg',
+    iconAlt: 'service-support-icon',
+    text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.',
+    points: [
+      {
+        title: 'operation support',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'Application support',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      },
+      {
+        title: 'STAFF AUGMENTATION',
+        text: 'While some software needs can be satisfied with modified off-the-shelf products, others require a custom software solution built from the ground up. With deep experience in web, and back-end platforms, our software solutions are scalable, flexible, and most importantly, secure.'
+      }
+    ]
+  }
+];
 
 function HomePage() {
   return (
@@ -16,6 +104,7 @@ function HomePage() {
         <h1>Bespoke software development solutions <br/><span>for your business</span></h1>
         <p>Design. Development. Consultancy.</p>
       </section>
+      {/*do we need it?*/}
       <section className={styles.pageNav}>
         <div>
           <a href="#development">development</a>
@@ -24,6 +113,31 @@ function HomePage() {
           <a href="#support">support</a>
         </div>
       </section>
+
+      {
+        data.map((service, i) => {
+          return (
+            <section key={i} id={service.anchor} className={cx(styles.section, styles[service.anchor])}>
+              <h3>{service.title}</h3>
+              <img src={service.icon} alt={service.iconAlt}/>
+              <p className={styles.serviceDescription}>{service.text}</p>
+              <div className={styles.servicePoints}>
+                {
+                  service.points.map((point, j) => {
+                    return (
+                      <div key={j}>
+                        <h5 className={styles.servicePointTitle}>{point.title}</h5>
+                        <p className={styles.servicePointDescription}>{point.text}</p>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </section>
+          )
+        })
+      }
+
       <BgShapeBottom/>
     </div>
   );
